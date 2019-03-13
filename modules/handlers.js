2e822e30171981813824f0e17a68d92348a2bb8d
Tomasz Rusin <tomek@rusin.info>
2019-03-13 22:56:41 +0100

    Resolved issue #1

--- modules/handlers.js
+++ modules/handlers.js
@@ -4,10 +4,9 @@ var fs = require('fs');
 var formidable = require('formidable');
 
 exports.upload = function(request, response) {
-    console.log("upload");
     var form = new formidable.IncomingForm();
     form.parse(request, function(error, fields, files) {
-        fs.renameSync(files.upload.path, "test.png");
+        fs.renameSync(files.upload.path, "./test.png");
         response.writeHead(200, {"Content-Type": "text/html"});
         response.write("received image:<br/>");
         response.write("<img src='/show' />");
@@ -15,8 +14,8 @@ exports.upload = function(request, response) {
     });
 }
 
+
 exports.welcome = function(request, response) {
-    console.log("welcome");
     fs.readFile('templates/start.html', function(err, html) {
         response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
         response.write(html);
@@ -25,14 +24,12 @@ exports.welcome = function(request, response) {
 }
 
 exports.error = function(request, response) {
-    console.log("err");
-    response.write("404 :(");
+    response.write("404 :("); 
     response.end();
 }
 
 exports.show = function(request, response) {
-    console.log('show');
-    fs.readFile("test.png", "binary", function(error, file) {
+    fs.readFile("./test.png", "binary", function(error, file) {
         response.writeHead(200, {"Content-Type": "image/png"});
         response.write(file, "binary");
         response.end();
